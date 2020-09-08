f3703cbb36ba90fc3411607fa6f890de64e71056
sahal <sahalelamkulam@gmail.com>
2020-09-08 20:32:45 +0530

    message

--- sss/soap/views.py
+++ sss/soap/views.py
@@ -1,16 +1,6 @@
 from django.shortcuts import render
 
 def index(request):
-    try:
-        email=request.POST["email"]
-        a=certificate.objects.get(email=email)
-        name=a.name.replace(" ","%20")
-        url="https://github.com/niranjanneeru/Pyweek-Temp/blob/master/assets/img/"+name+".png?raw=true"
-        return render(request,'soap/base.html',{"url":url})
-    except:
-        pass
-    """ return render(request,'soap/no_user.html')"""
-
     return render(request,'soap/hello.html')
 
 def git_fetch(request):
